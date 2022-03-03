4fae0d59e643c1233377bd88a2a58ca88c289417
LucaLuz <luiza24021@gmal.com>
2022-03-03 16:08:30 -0300

    commit 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContatComponent } from './views/contat/contat.component';
+import { HomeComponent } from './views/home/home.component';
+import { JogadorComponent } from './views/jogador/jogador.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component:HomeComponent},
+  {path:"contat" , component: ContatComponent},
+  {path:"jogador" , component: JogadorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContatComponent } from './views/contat/contat.component';
+import { JogadorComponent } from './views/jogador/jogador.component';
+import { HomeComponent } from './views/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContatComponent,
+    JogadorComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
